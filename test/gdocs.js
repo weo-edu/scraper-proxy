6e1884d0957f94f8666292067a3339f33641ec0f
Andrew Shaffer <andrew@weo.io>
2015-01-23 15:57:45 -0800

    update

--- test/gdocs.js
+++ test/gdocs.js
@@ -9,6 +9,8 @@ function scrape(url, cb) {
 }
 
 describe('google docs', function() {
+  this.timeout(5000);
+
   var pdfUrl = 'https://docs.google.com/file/d/1rNGkMLyovbD5vfx8QYMCweJJzm9BQPEo6-op4P20sA2K9P-_oH1_XD9N58MM/edit?usp=sharing';
   var docUrl = 'https://docs.google.com/document/d/1DUH6nU7FnIVB3SSq8YIQ4xAdjUeGP9o5tAcU97mRUJk/edit?usp=sharing';
 
@@ -16,17 +18,19 @@ describe('google docs', function() {
     scrape(docUrl, function(err, data) {
       expect(err).to.be.null;
       expect(data.thumbnail_url).to.not.be.falsy;
+      console.log('data', data.thumbnail_url);
       expect(data.title).to.equal('Testing Public Document WEO');
       expect(data.html).to.equal('<iframe src="https://docs.google.com/document/d/1DUH6nU7FnIVB3SSq8YIQ4xAdjUeGP9o5tAcU97mRUJk/preview"></iframe>');
       done();
     })
   });
 
-  it('should work with pdfs', function(done) {
+  it.only('should work with pdfs', function(done) {
     scrape(pdfUrl, function(err, data) {
       expect(err).to.be.null;
       expect(data.thumbnail_url).to.not.be.falsy;
       expect(data.title).to.equal('vidsheet exponents.pdf');
+      console.log('data', data);
       expect(data.html).to.equal('<iframe src=\"https://docs.google.com/file/d/1rNGkMLyovbD5vfx8QYMCweJJzm9BQPEo6-op4P20sA2K9P-_oH1_XD9N58MM/edit?usp=drivesdk\"></iframe>');
       done();
     });
