4193936f4e783a2b052ad7373ff9d3797a8ab88c
Andrew Shaffer <darawk@gmail.com>
2015-06-17 17:48:11 -0700

    upload all images to s3 and normalize their dimensions

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-var express = require('express');
-var app = module.exports = express();
-var config = require('./lib/config');
+var express = require('express')
+var app = module.exports = express()
+var config = require('./lib/config')
 
 /*
   Logging
  */
-require('weo-logger')('scraper');
-app.use(require('morgan')('tiny'));
+require('weo-logger')('scraper')
+app.use(require('morgan')('tiny'))
 
 /*
   Config
  */
-app.configure('development', function() {
-  app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
-});
+app.configure('development', function () {
+  app.use(express.errorHandler({dumpExceptions: true, showStack: true}))
+})
 
-app.configure('production', function() {
-  app.use(express.errorHandler());
-});
+app.configure('production', function () {
+  app.use(express.errorHandler())
+})
 
-app.use(require('cors')());
+app.use(require('cors')())
 
 /*
   Listen
  */
-app.listen(config.port, function() {
-  console.log('listening', config.port);
-});
+app.listen(config.port, function () {
+  console.log('listening', config.port)
+})
 
 /*
   Boot
  */
-app.use(require('./lib/main'));
+app.use(require('./lib/main'))
 
-app.use(function(err, req, res, next) {
-  if(err) {
-    if(err.code !== 400) console.log('err', err, err.stack);
-    res.send(err.code || err.error_code || 500, err);
+app.use(function (err, req, res, next) {
+  if (err) {
+    if (err.code !== 400) console.log('err', err, err.stack)
+    res.send(err.code || err.error_code || 500, err)
   } else
-    next();
-});
+    next()
+})
--- test/gdocs.js
+++ test/gdocs.js
@@ -1,38 +1,38 @@
-var expect = require('chai').expect;
-var app = require('../');
-var request = require('supertest')(app);
+var expect = require('chai').expect
+var app = require('../')
+var request = require('supertest')(app)
 
-function scrape(url, cb) {
+function scrape (url, cb) {
   request
     .get('/1/oembed?url=' + encodeURIComponent(url))
-    .end(cb);
+    .end(cb)
 }
 
-describe('google docs', function() {
-  this.timeout(5000);
+describe('google docs', function () {
+  this.timeout(5000)
 
-  var pdfUrl = 'https://docs.google.com/file/d/1rNGkMLyovbD5vfx8QYMCweJJzm9BQPEo6-op4P20sA2K9P-_oH1_XD9N58MM/edit?usp=sharing';
-  var docUrl = 'https://docs.google.com/document/d/1DUH6nU7FnIVB3SSq8YIQ4xAdjUeGP9o5tAcU97mRUJk/edit?usp=sharing';
+  var pdfUrl = 'https://docs.google.com/file/d/1rNGkMLyovbD5vfx8QYMCweJJzm9BQPEo6-op4P20sA2K9P-_oH1_XD9N58MM/edit?usp=sharing'
+  var docUrl = 'https://docs.google.com/document/d/1DUH6nU7FnIVB3SSq8YIQ4xAdjUeGP9o5tAcU97mRUJk/edit?usp=sharing'
 
-  it('should work with normal docs', function(done) {
-    scrape(docUrl, function(err, res) {
-      var data = res.body;
-      expect(err).to.be.null;
-      expect(data.thumbnail_url).to.not.be.falsy;
-      expect(data.title).to.equal('Testing Public Document WEO');
-      expect(data.html).to.equal('<iframe src="https://docs.google.com/document/d/1DUH6nU7FnIVB3SSq8YIQ4xAdjUeGP9o5tAcU97mRUJk/preview"></iframe>');
-      done();
+  it('should work with normal docs', function (done) {
+    scrape(docUrl, function (err, res) {
+      var data = res.body
+      expect(err).to.be.null
+      expect(data.thumbnail_url).to.not.be.falsy
+      expect(data.title).to.equal('Testing Public Document WEO')
+      expect(data.html).to.equal('<iframe src="https://docs.google.com/document/d/1DUH6nU7FnIVB3SSq8YIQ4xAdjUeGP9o5tAcU97mRUJk/preview"></iframe>')
+      done()
     })
-  });
+  })
 
-  it('should work with pdfs', function(done) {
-    scrape(pdfUrl, function(err, res) {
-      var data = res.body;
-      expect(err).to.be.null;
-      expect(data.thumbnail_url).to.not.be.falsy;
-      expect(data.title).to.equal('vidsheet exponents.pdf');
-      expect(data.html).to.equal('<iframe src=\"https://docs.google.com/file/d/1rNGkMLyovbD5vfx8QYMCweJJzm9BQPEo6-op4P20sA2K9P-_oH1_XD9N58MM/edit?usp=drivesdk\"></iframe>');
-      done();
-    });
-  });
-});
+  it('should work with pdfs', function (done) {
+    scrape(pdfUrl, function (err, res) {
+      var data = res.body
+      expect(err).to.be.null
+      expect(data.thumbnail_url).to.not.be.falsy
+      expect(data.title).to.equal('vidsheet exponents.pdf')
+      expect(data.html).to.equal('<iframe src="https://docs.google.com/file/d/1rNGkMLyovbD5vfx8QYMCweJJzm9BQPEo6-op4P20sA2K9P-_oH1_XD9N58MM/edit?usp=drivesdk"></iframe>')
+      done()
+    })
+  })
+})
