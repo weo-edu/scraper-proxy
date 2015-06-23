e146885aff79b706f80a0e92f9feda0a5f072292
Andrew Shaffer <andrew@weo.io>
2015-06-23 16:11:48 -0700

    swap index.js and server.js; add graphicsmagick to Dockerfile

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-require('babel/register')
-module.exports = require('./server')
+/**
+ * Modules
+ */
+import koa from 'koa'
+import koaLogger from 'koa-logger'
+import cors from 'kcors'
+import mount from 'koa-mount'
+import weoLogger from 'weo-logger'
+import {port} from './lib/config'
+import main from './lib/main'
+import handleError from './lib/handleError'
+
+/**
+ * Vars
+ */
+const app = koa()
+
+/**
+ * Exports
+ */
+export default app
+
+
+/*
+  Logging
+ */
+weoLogger('scraper')
+app.use(koaLogger())
+
+/**
+ * CORS
+ */
+app.use(cors())
+
+/*
+  Main app
+ */
+app.use(mount(main))
+
+/*
+  Listen
+ */
+app.listen(port, () => {
+  console.log('listening', port)
+})
